a515f1ae6c22350dc4b0eb42f224799a71ba9a1c
BackinR2D2 <cristimlg334@gmail.com>
2021-04-02 13:43:00 +0300

    v2

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
-
+const path = require('path');
+const axios = require('axios');
 const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: 'localhost',
+        routes: {
+            files: {
+                relativeTo: path.join(__dirname, 'public'),
+            }
+        }
+    });
+
+    await server.register(require('@hapi/inert'));
+    await server.register(require('@hapi/vision'));
+
+    server.views({
+        engines: {
+            ejs: require('ejs')
+        },
+        relativeTo: __dirname,
+        path: 'views',
+    })
+
+    server.route({
+        method: 'POST',
+        path: '/movie',
+        handler: async function (req, res) {
+            try {
+                const movieSearch = req.payload.movie;
+                const { data } = await axios.get(`http://www.omdbapi.com/?t=${movieSearch}&apikey=${process.env.API_KEY}`);
+                return res.view('name', { data });
+            } catch (error) {
+                return res.file('error.html');
+            }
+        }
     });
 
     server.route({
         method: 'GET',
         path: '/',
-        handler: (request, h) => {
-
-            return 'Hello World!';
+        handler: (req, res) => {
+            return res.file('index.html');
         }
     });
 
